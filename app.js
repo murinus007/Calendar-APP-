2df7fed8be980246c9ed5c53259b80b1c3f03b95
hesterocratesHercules <sergeevalexeyalekseevich@gmail.com>
2021-04-16 14:00:45 +0300

    add Procfile

--- app.js
+++ app.js
@@ -19,6 +19,10 @@ app.use(morgan("dev"));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+app.listen(PORT, () => {
+  console.log('Server has been started...')
+})
+
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   res.header(
